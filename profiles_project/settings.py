db983f4c121559c3d1c554893ec13673097b5afc
subrata153 <subrata.goswami153@gmail.com>
2020-08-31 00:16:44 +0530

    Added custom user models

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#custom models for default user settings
+
+AUTH_USER_MODEL = 'profile_api.UserProfile'
