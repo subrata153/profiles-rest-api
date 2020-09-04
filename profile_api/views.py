f66351d1faa1dd6ba8e2facd344ddb9ff70b0a9e
subrata153 <subrata.goswami153@gmail.com>
2020-09-04 17:03:54 +0530

    remove test

--- profile_api/views.py
+++ profile_api/views.py
@@ -115,5 +115,3 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-
-testsert    
